b9fadee215c424120a3c8b2d06bb44c6d4c70786
LynxQuas <lynxquas@gmail.com>
2024-11-29 06:02:19 +0000

    fix(styles): removed main layout's padding

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -7,7 +7,7 @@ const Root = () => {
             <header>
                 <Navigations />
             </header>
-            <main className="md:p-8 mt-[4rem] md:mt-[0rem] flex-grow bg-[#eee] min-h-screen md:w-[70%] md:ml-[18rem]">
+            <main className="mt-[4rem] md:mt-[0rem] flex-grow bg-[#eee] min-h-screen md:w-[70%] md:ml-[18rem]">
                 <Outlet />
             </main>
         </div>
