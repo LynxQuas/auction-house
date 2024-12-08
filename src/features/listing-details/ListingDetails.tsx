4a5bd1bc7bad0cf5559de2b63161222a0a19942d
LynxQuas <lynxquas@gmail.com>
2024-12-08 17:34:48 +0000

    chore: split modal's component
    Signed-off-by: LynxQuas <lynxquas@gmail.com>

--- src/features/listing-details/ListingDetails.tsx
+++ src/features/listing-details/ListingDetails.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import Modal from "../../components/Modal";
+import CommentModal from "../../components/modals/CommentModal";
 
 const ListingDetails = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -12,22 +13,7 @@ const ListingDetails = () => {
         <>
             {isModalOpen && (
                 <Modal onClose={() => setIsModalOpen(false)}>
-                    <div className="flex gap-8 flex-col absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-                        <div className="flex items-center gap-4">
-                            <div className="w-10 h-10 bg-black rounded-full" />
-                            <span>Username</span>
-                        </div>
-
-                        <textarea
-                            className="border-2 resize-none px-4 py-2"
-                            rows={5}
-                            cols={40}
-                            placeholder="Comment"
-                        ></textarea>
-                        <button className="bg-black text-white py-2 rounded-md">
-                            Comment
-                        </button>
-                    </div>
+                    <CommentModal />
                 </Modal>
             )}
             <div>
