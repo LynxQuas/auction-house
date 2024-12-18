e0c8117cdf0b44cfe10942a19f7dcdfb186af6a2
LynxQuas <lynxquas@gmail.com>
2024-12-18 06:56:03 +0000

    fix: icon to button
    Signed-off-by: LynxQuas <lynxquas@gmail.com>

--- src/components/ModalLayout.tsx
+++ src/components/ModalLayout.tsx
@@ -16,10 +16,9 @@ const ModalLayout = ({ onClose, children }: ModalProps) => {
                 className="absolute top-[50%] left-[50%] translate-x-[-50%] md:translate-x-[-30%] translate-y-[-50%] w-[90%] h-[30rem] md:w-[30rem] md:h-[30rem] bg-white shadow-lg rounded-md"
             >
                 {/* Change this to icon  */}
-                <IoMdClose
-                    onClick={onClose}
-                    className="absolute top-0 right-0 m-5 w-6 h-6 cursor-pointer"
-                />
+                <button onClick={onClose}>
+                    <IoMdClose className="absolute top-0 right-0 m-5 w-6 h-6 cursor-pointer" />
+                </button>
                 {children}
             </div>
         </div>
